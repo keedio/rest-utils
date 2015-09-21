82a0fcf3e400b1e0bf7a650705d03f3777b2cff3
Rodrigo Olmo Velasco <rolmo@MacBook-Pro-de-Rodrigo.local>
2015-09-21 11:11:35 +0200

    Fixes

--- core/src/main/java/io/confluent/rest/Application.java
+++ core/src/main/java/io/confluent/rest/Application.java
@@ -18,6 +18,16 @@ package io.confluent.rest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.jaxrs.base.JsonParseExceptionMapper;
 
+import org.eclipse.jetty.jaas.JAASLoginService;
+import org.eclipse.jetty.jaas.spi.AbstractLoginModule;
+import org.eclipse.jetty.jaas.spi.UserInfo;
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.DefaultIdentityService;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.LoginService;
+import org.eclipse.jetty.security.SecurityHandler;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
@@ -26,6 +36,8 @@ import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.server.handler.RequestLogHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.security.Constraint;
+import org.eclipse.jetty.util.security.Credential;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.server.validation.ValidationFeature;
@@ -122,6 +134,7 @@ public abstract class Application<T extends RestConfig> {
     server.setConnectors(new Connector[]{connector});
 
     ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.setSecurityHandler(basicAuth("ldaploginmodule"));
     context.setContextPath("/");
     context.addServlet(servletHolder, "/*");
 
@@ -222,5 +235,33 @@ public abstract class Application<T extends RestConfig> {
    */
   public void onShutdown() {
   }
+
+  private final SecurityHandler basicAuth(String realm) {
+
+      JAASLoginService l = new JAASLoginService();
+      l.setName("Test JAAS Realm");
+      l.setLoginModuleName("ldaploginmodule");
+      //l.setIdentityService(new DefaultIdentityService());
+      //l.setRoleClassNames(new String[]{"org.eclipse.jetty.plus.jaas.JAASRole"});
+      
+      Constraint constraint = new Constraint();
+      constraint.setName(Constraint.__BASIC_AUTH);
+      constraint.setRoles(new String[]{"onemanagers"});
+      constraint.setAuthenticate(true);
+       
+      ConstraintMapping cm = new ConstraintMapping();
+      cm.setConstraint(constraint);
+      cm.setPathSpec("/*");
+      
+      ConstraintSecurityHandler csh = new ConstraintSecurityHandler();
+      csh.setAuthenticator(new BasicAuthenticator());
+      csh.addConstraintMapping(cm);
+      csh.setLoginService(l);
+      csh.setRealmName("realm");
+      
+      return csh;
+    
+  }
+  
 }
 
